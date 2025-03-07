11d8a38398f07cf3084ca97a2b8d6780c3609424
Arnaud PY <arnaud.py@student-cs.fr>
2025-03-07 11:14:15 +0100

    Backend server update

---
+++ backend/app/app.py
@@ -0,0 +1,87 @@
+from datetime import datetime
+from flask import Flask, request
+from functions.db_functions import get_userID, get_user_last_research_date, get_user_providers, modify_user_providers, get_user_results, modify_last_research_user, modify_film
+from functions.fetch_functions import get_ids, get_providers, get_watchlist, get_all_regions, region_providers, sort_watchlist
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+region_list = get_all_regions()
+
+@app.route('/', methods=['GET'])
+def get_home():
+    return "Boum boum, server is running !"
+    
+@app.route('/your_providers', methods=['GET'])
+def get_your_providers():
+    username = request.args.get('username')
+    country_code = request.args.get('country_code')
+    if not country_code:
+        return "Error: country_code parameter is required", 400
+    if not username:
+        return "Error: username parameter is required", 400
+    if country_code not in region_list:
+        return "Error: invalid country_code", 400
+    
+    user_ID = get_userID(username)
+    if user_ID == -1:
+        return "Error: failed to retrieve user ID", 500
+    
+    providers = get_user_providers(user_ID,country_code)
+    
+    return {"providers": providers}
+    
+@app.route('/region_providers', methods=['GET'])
+def get_region_providers():
+    region_list = get_all_regions()
+    country_code = request.args.get('country_code')
+    if not country_code:
+        return "Error: country_code parameter is required", 400
+    if country_code not in region_list:
+        return "Error: invalid country_code", 400
+
+    return {"country_code" : country_code, "providers": region_providers(country_code)}
+    
+@app.route('/regions', methods=['GET'])
+def get_regions():
+    regions = get_all_regions()
+    return {"regions": regions}
+    
+@app.route('/results', methods=['POST'])
+def results(): 
+    data = request.json
+    username = data.get('username')
+    country_code = data.get('country_code')
+    selected_providers = data.get('providers', [])
+    if not country_code:
+        return "Error: country_code parameter is required", 400
+    if not username:
+        return "Error: username parameter is required", 400
+    if country_code not in region_list:
+        return "Error: invalid country_code", 400
+
+    user_ID = get_userID(username)
+    if user_ID == -1:
+        return "Error: failed to retrieve user ID", 500
+    
+    last_research_date = get_user_last_research_date(user_ID)
+    if last_research_date == -1:
+        return "Error: failed to retrieve the last research date", 500
+    
+    modify_user_providers(user_ID,country_code,selected_providers)    
+    
+    watchlist = get_user_results(user_ID,country_code)
+    if not watchlist or not last_research_date or (datetime.now() - last_research_date).days > 7:
+        watchlist = get_watchlist(username)
+        if not watchlist:
+            return "Error: failed to retrieve the watchlist", 500
+        watchlist = get_ids(watchlist)
+        watchlist = get_providers(watchlist)
+        watchlist = sort_watchlist(watchlist, selected_providers)
+        modify_last_research_user(user_ID)
+        modify_film(user_ID,country_code,watchlist)
+    elif any (film["providers"] not in selected_providers for film in watchlist):
+        watchlist = sort_watchlist(watchlist, selected_providers)    
+    
+    return watchlist
---
+++ backend/functions/fetch_functions.py
@@ -0,0 +1,129 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import quote
+
+headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYzUwMDE0OGU5ODYxMGFjNjI4ZTc3N2QxMTgwZDQxNyIsIm5iZiI6MTczNzU1NTg1Mi40OTcsInN1YiI6IjY3OTBmZjhjYmE0Njk5MGY1NGRmZjZhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oUTZhoCrugczQbR02PA2h-ZGOpSjR5EIS9IL6nva63E"
+    }
+
+def get_watchlist(username):
+    """
+    Retrieve the watchlist of a Letterboxd user.
+    """
+    watchlist = []
+    page = 1
+    
+    while True:
+        url = f'https://letterboxd.com/{username}/watchlist/page/{page}/'
+        response = requests.get(url)
+        
+        if response.status_code != 200:
+            raise Exception(f"Failed to retrieve watchlist for user {username}. HTTP Status Code: {response.status_code}")
+        
+        soup = BeautifulSoup(response.text, 'html.parser')
+        films = soup.find_all('li', class_='poster-container')
+
+        if not films:
+            break
+        
+        for film in films:
+            title = film.find('img')['alt']
+            infos = film.find('div', class_='film-poster')['data-film-slug']
+            if len(infos.split('-')[-1]) == 4 and infos.split('-')[-1].isnumeric() and title[-4:] != infos.split('-')[-1]:
+                date = int(infos.split('-')[-1])
+                watchlist.append({'title': title, 'date': date})
+            else:
+                watchlist.append({'title': title})
+        
+        page += 1
+    
+    return watchlist
+
+def get_ids(watchlist):
+    """
+    Retrieve the film id and note from The Movie Database API.
+    """
+    indexed_watchlist = []
+    for index,film in enumerate(watchlist):
+        title_url_compatible = quote(film["title"])
+        if 'date' in film:
+            response = requests.get(f'https://api.themoviedb.org/3/search/movie?query={title_url_compatible}&primary_release_year={film["date"]}', headers=headers)
+        else:
+            response = requests.get(f'https://api.themoviedb.org/3/search/movie?query={title_url_compatible}', headers=headers)
+            
+        if response.status_code != 200:
+            raise Exception(f"Failed to retrieve the film id for '{film}'. HTTP Status Code: {response.status_code}")
+        
+        data = response.json()
+        if data['results']:
+            watchlist[index]['id'] = data['results'][0]['id']
+            watchlist[index]['note'] = round(data['results'][0]['vote_average'], 1)
+            indexed_watchlist.append(watchlist[index])
+        indexed_watchlist.sort(key=lambda x: x.get('note', 0), reverse=True)
+        
+    return indexed_watchlist
+   
+def get_providers(watchlist):
+    """
+    Retrieve the film providers from The Movie Database API.
+    """
+    for index,film in enumerate(watchlist):
+        id = int(film["id"])
+        response = requests.get(f'https://api.themoviedb.org/3/movie/{id}/watch/providers', headers=headers)
+        
+        if response.status_code != 200:
+            raise Exception(f"Failed to retrieve the film providers for '{film}'. HTTP Status Code: {response.status_code}")
+        
+        providers = []
+        data = response.json()
+        if "results" in data and 'FR' in data["results"] and 'flatrate' in data["results"]['FR']:
+            flatrate = data["results"]['FR']['flatrate']
+            for provider in flatrate:
+                providers.append(provider['provider_name'])
+        watchlist[index]['providers'] = providers
+        
+    return [film for film in watchlist if film["providers"]]
+
+def sort_watchlist(watchlist,providers):
+    """
+    Sort the watchlist by providers.
+    """
+    sorted_watchlist = []
+    for film in watchlist:
+        new_providers = [provider for provider in film["providers"] if provider in providers]
+        if new_providers:
+            film["providers"] = new_providers
+            sorted_watchlist.append(film)
+    return sorted_watchlist
+ 
+def region_providers(country_code):
+    """
+    Retrieve the movie streaming platform for a specific region.
+    """
+    response = requests.get(f"https://api.themoviedb.org/3/watch/providers/movie?watch_region={country_code}", headers=headers)
+    if response.status_code != 200 :
+        raise Exception(f"Impossible to retrieve the movie streaming platform for your region : {country_code}")
+
+    region_providers = []
+    data = response.json()
+    if data["results"]:
+        response_providers = data["results"]
+        for response_provider in response_providers:
+            if "display_priorities" in response_provider and country_code in response_provider["display_priorities"]:
+                region_providers.append(response_provider["provider_name"])
+                
+    return region_providers
+
+def get_all_regions():
+    """
+    Retrieve all regions from The Movie Database API.
+    """
+    response = requests.get("https://api.themoviedb.org/3/watch/providers/regions?language=en-US", headers=headers)
+    if response.status_code != 200:
+        raise Exception(f"Failed to retrieve the regions. HTTP Status Code: {response.status_code}")
+    data = response.json()
+    regions = []
+    for region in data["results"]:
+        regions.append(region["iso_3166_1"])
+    return regions
