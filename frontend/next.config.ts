fce96fcbe7f61eae7284ec2f11cd98d6ef32ed0e
Arnaud PY <arnaud.py@student-cs.fr>
2025-03-12 23:09:55 +0100

    Resolving cache size error

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  reactStrictMode: true
+  reactStrictMode: true,
+  compress: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
